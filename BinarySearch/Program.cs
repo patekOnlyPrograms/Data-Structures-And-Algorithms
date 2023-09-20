514c5efb64c63355fb35a056a4de2ce4cb82458d
Susruth <abhirama99@live.com>
2023-09-20 12:51:24 +0100

    initial commit

---
+++ BinarySearch/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace BinarySearch;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<int> array = new List<int>() { 1, 7, 6, 8, 6, 9 };
+        array.Sort();
+        foreach (var item in array)
+        {
+            Console.WriteLine(item);
+        }
+
+        Console.WriteLine(BinarySearch(array,9));
+    }
+
+    public static int BinarySearch(List<int> sortedArray, int item)
+    {
+        var low = sortedArray[0];
+        var high = sortedArray.Count - 1;
+
+        //Repeat till the low and high points meet into the middle which shows
+        //that the item was in the middle
+        while (low <= high)
+        {
+            var mid = (low + high) / 2;
+            var guess = sortedArray[mid];
+            if (guess == item)
+            {
+                return mid;
+            }
+            if (guess > item)
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
---
+++ SelectionSort/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace MyNamespace;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var data = new List<int>{ 1, 5, 6, 8, 9, 90,7 };
+        SelectionSortClass ss = new SelectionSortClass();
+        ss.SelectionSort(data);
+    }
+}
+
+public class SelectionSortClass
+{
+    public void SelectionSort(List<int> unsortedArray)
+    {
+        foreach (var item in unsortedArray)
+        {
+            Console.Write(item);
+        }
+        Console.WriteLine(Environment.NewLine);
+        //selection sort starts here
+        
+        for (int i = 0; i < unsortedArray.Count - 1; i++)
+        {
+            var minimum = i; //set minimum as the first element
+            for (int j = i+1; j < unsortedArray.Count; j++) //go through the list to find the next element
+            {
+                if (unsortedArray[j] < unsortedArray[minimum]) //if item on the right is less than the item on left
+                {
+                    minimum = j; //set minimum as the right
+                }
+            }
+            //swap
+            var temp = unsortedArray[i]; //create temp variable to store first
+            unsortedArray[i] = unsortedArray[minimum]; //
+            unsortedArray[minimum] = temp;
+        }
+        
+        foreach (var item in unsortedArray)
+        {
+            Console.Write(item);
+        }
+    }
+}
